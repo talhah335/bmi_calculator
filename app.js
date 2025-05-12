f5af07d39e3eb479693d3a7cb3d8b4aaba63bc2e
Talha Hassan <talha.hasan335@gmail.com>
2025-05-12 15:28:51 -0600

    created a bmi calculation for both metric and imperial

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
 window.onload = () => {
-    let button = document.querySelector("#btn");
+    document.getElementById("defaultOpen").click();
 
-    button.addEventListener("click", calculateBMI);
+    document.querySelector("#metric-btn").addEventListener("click", () => calculateBMI("Metric"));
+    document.querySelector("#imperial-btn").addEventListener("click", () => calculateBMI("Imperial"));
 };
 
-function calculateBMI() {
+function calculateBMI(mode) {
+    let height, weight, result;
 
-    let height = parseInt(document
-        .querySelector("#height").value);
-
-    let weight = parseInt(document
-        .querySelector("#weight").value);
-
-    let result = document.querySelector("#result");
-
-    if (height === "" || isNaN(height))
-        result.innerHTML = "Provide a valid Height!";
-
-    else if (weight === "" || isNaN(weight))
-        result.innerHTML = "Provide a valid Weight!";
-
-    else {
-
-        let bmi = (weight / ((height * height)
-            / 10000)).toFixed(2);
-
-        if (bmi < 18.6) result.innerHTML =
-            `Under Weight : <span>${bmi}</span>`;
-
-        else if (bmi >= 18.6 && bmi < 24.9)
-            result.innerHTML =
-                `Normal : <span>${bmi}</span>`;
-
-        else result.innerHTML =
-            `Over Weight : <span>${bmi}</span>`;
+    if (mode === "Metric") {
+        height = parseInt(document.querySelector("#metric-height").value);
+        weight = parseInt(document.querySelector("#metric-weight").value);
+        result = document.querySelector("#metric-result");
+    } else if (mode === "Imperial") {
+        height = parseInt(document.querySelector("#imperial-height").value);
+        weight = parseInt(document.querySelector("#imperial-weight").value);
+        result = document.querySelector("#imperial-result");
     }
+
+    if (!height || isNaN(height)) {
+        result.innerHTML = "Provide a valid Height!";
+    } else if (!weight || isNaN(weight)) {
+        result.innerHTML = "Provide a valid Weight!";
+    } else {
+        let bmi;
+        if (mode === "Metric") {
+            bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        } else if (mode === "Imperial") {
+            bmi = ((weight / (height * height)) * 703).toFixed(2);
+        }
+
+        if (bmi < 18.5) {
+            result.innerHTML = `Under Weight : <span>${bmi}</span>`;
+        } else if (bmi >= 18.5 && bmi < 25) {
+            result.innerHTML = `Normal Weight : <span>${bmi}</span>`;
+        } else {
+            result.innerHTML = `Over Weight : <span>${bmi}</span>`;
+        }
+    }
+}
+
+function openMode(evt, unitMode) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(unitMode).style.display = "block";
+    evt.currentTarget.className += " active";
 }
